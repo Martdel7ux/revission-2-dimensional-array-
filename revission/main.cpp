1c38d0f94b69bfc1e15556ceac07b1a0fa4b5838
Martin Ahoto <martinahoto4@gmail.com>
2021-11-06 18:07:36 +0200

    Initial Commit

---
+++ revission/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  revission
+//
+//  Created by MARTIN on 06/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
